5453212f01468bf7242c5cb3af6fe6df6db3cb4e
Vin Le <letandung.soft@gmail.com>
2025-09-20 16:55:32 +0700

    Add code header

--- template-parts/header/header-five.php
+++ template-parts/header/header-five.php
@@ -1,3 +1,4 @@
+<!-- header/header-five.php -->
 <div class="coblog-topbar coblog-five-topbar">
     <div class="container">
         <div class="coblog-flex-wrap">
@@ -17,16 +18,29 @@
         <!--/.row-->
     </div><!-- .container -->
 </div>
-<header id="masthead" class="site-header coblog-header-five">
+<header id="masthead" class="site-header coblog-header-five coblog-header-custom">
     <div class="main-navigation-wrap site-branding d-none d-lg-block">
         <div class="container">
             <div class="coblog-flex-wrap coblog-menu-wrap">
                 <div class="coblog-site-branding coblog-flex-col">
-                    <?php get_template_part('template-parts/logo'); ?>
+                    <div class="logo-background-wrapper">
+                        <div class="logo-gradient-layer"></div>
+                        <div class="logo-content">
+                            <?php get_template_part('template-parts/logo'); ?>
+                        </div>
+                    </div>
                 </div><!-- .site-branding -->
                 <?php get_template_part('template-parts/main-menu'); ?>
                 <div class="coblog-flex-col coblog-search-user-cart">
                     <?php get_template_part('template-parts/header/cart-search-user'); ?>
+                </div>
+                <!-- Hamburger Menu -->
+                <div class="coblog-flex-col coblog-hamburger-menu">
+                    <button class="hamburger-menu-btn" type="button" aria-label="Toggle Menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
                 </div>
             </div>
         </div><!-- .container -->
@@ -40,4 +54,25 @@
             </div><!-- Site search end -->
         </div><!-- Site search end -->
     <?php } ?>
+
+    <!-- Hamburger Menu Content -->
+    <div class="coblog-hamburger-content" style="display: none;">
+        <div class="container">
+            <div class="hamburger-menu-inner">
+                <?php
+                if (has_nav_menu('hamburger-menu')) {
+                    wp_nav_menu(array(
+                        'theme_location' => 'hamburger-menu',
+                        'container'      => 'nav',
+                        'container_class' => 'hamburger-navigation',
+                        'menu_class'     => 'hamburger-menu-list',
+                        'fallback_cb'    => false,
+                    ));
+                } else {
+                    echo '<p>Vui lòng tạo menu "Hamburger Menu" trong Appearance > Menus</p>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </header><!-- #masthead -->
